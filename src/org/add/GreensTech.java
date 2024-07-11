dccf9120e76d602636680a1ae6cedc83c8369ec0
DevElan2392 <devatusker@gmail.com>
2024-07-11 12:00:51 +0530

    new code

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+	   System.out.println("Address is OMR");
+	}
+    private void greensAdayar() {
+	   System.out.println("Address is Adayar");
+    }
+    private void greensTambaram() {
+	   System.out.println("Address is Tambaram");
+    }
+    private void greensVelacherry() {
+	   System.out.println("Address is Velacherry");
+    }
+    private void greensAnnanagar() {
+	   System.out.println("Address is Annanagar");
+    }
+    public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greensVelacherry();
+		g.greensAnnanagar();
+	}
+    }
